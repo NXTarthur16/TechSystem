d22ce70cf305c627209054bc93b5cd85c9d968d2
PROGRAMADOR JAVA  11612841.2023.1 <116128412023.1@senacrjedu.local>
2023-10-04 18:15:07 -0300

    atualizacao-1

--- src/main/java/senac/java/Domain/FuncionarioMock.java
+++ src/main/java/senac/java/Domain/FuncionarioMock.java
@@ -3,7 +3,7 @@ package senac.java.Domain;
 public class FuncionarioMock {
     public static String completeName = "";
     public static String cpf = "01984178056";
-    public static String email = "amanda01rj@gmail.com";
+    public static String email = "user01rj@gmail.com";
     public static String address = "Rua: Belavista nº20, Botafogo";
     public static String password = "axy0125874";
 }
--- src/main/java/senac/java/Domain/FuncionarioMock2.java
+++ src/main/java/senac/java/Domain/FuncionarioMock2.java
@@ -4,7 +4,7 @@ public class FuncionarioMock2 {
     public static String completeName2 = "Rafael Ramos";
     public static String cpf2 = "12345678910";
     public static String email2 = "RafaelRamos@gmail.com";
-    public static String address2 = "Pimenteiras, Rua guandu 690";
+    public static String address2 = "Barra";
     public static String password2 = "axy123456";
 
 }
